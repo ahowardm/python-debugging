720b917ea22a3f480ebaa050bb31bb2bbb1ca02f
Andres Howard <ahowardm@gmail.com>
2021-09-13 23:26:30 -0300

    feat: add loggging to main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 from numpy import random
 
 from match import Match
@@ -21,48 +23,70 @@ def players_u():
     return players
 
 def create_team_1(players):
-    return Team("el colo", "Santiago", "blanco", players)
+    logging.info(f"Creating team with players {players}")
+    team = Team("el colo", "Santiago", "blanco", players)
+    logging.info(f"Team created succesfully with players {players}")
+    return team
 
 def create_team_2(players):
-    return Team("la chile", "Santiago", "azul", players)
+    logging.info(f"Creating team with players {players}")
+    team = Team("la chile", "Santiago", "azul", players)
+    logging.info(f"Team created succesfully with players {players}")
+    return team
 
 def create_match():
-    # players_colo = players_colo()
-    # players_u = players_u()
+    logging.info('Starting to create a match')
     team_1 = create_team_1(players_colo())
     team_2 = create_team_2(players_u())
-    return Match(team_1, team_2)
+    match =  Match(team_1, team_2)
+    logging.info('Match created succesfully')
+    return match
 
 def change_in_team(players):
     i = random.randint(0, len(players) - 1)
     players[i] = Player('Marcelo', 'Salas')
 
 def print_match_overview(match):
+    logging.info('Printing match overview')
     print('*** Descripción del partido ***')
     print(match)
+    logging.info('Match overview printed succesfully')
 
 def print_match_details(match):
+    logging.info('Printing match details')
     print('\n*** Detalles del partido ***')
     print(match.team_1)
     print(match.team_2)
     print(f"Fecha: {match.day}")
+    logging.info('Match details printed succesfully')
 
 def print_score(match):
+    logging.info('Printing match score')
     print('\n *** Marcador ***')
     match.print_scores()
     match.day = "Hoydía"
     match.goal(match.team_1)
     match.print_scores()
+    logging.info('Match score printed succesfully')
 
 def print_players(match):
+    logging.info('Printing match players')
     print('\n*** Jugadores ***')
     print(match.team_1.players)
     change_in_team(match.team_1.players)
     print(match.team_1.players)
+    logging.info('Match players printed succesfully')
+
+def logger_config():
+    logging.basicConfig(filename='soccer.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
-match = create_match()
-print_match_overview(match)
-print_match_details(match)
-print_score(match)
-print_players(match)
+if __name__ == '__main__':
+    logger_config()
+    logging.info('Program Started')
+    match = create_match()
+    print_match_overview(match)
+    print_match_details(match)
+    print_score(match)
+    print_players(match)
+    logging.info('Program Finished')
